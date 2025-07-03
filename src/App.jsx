95b8e916a97826296351002590ba3cfde4f08572
Prerna <mishraprerna465@gmail.com>
2025-07-03 18:54:55 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h2>This application is deployed using CI/CD</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
